8a2ba70a943708fa48e01289f32a4d7dc0305b14
frank <chl179@ucsd.edu>
2024-03-04 11:20:29 -0800

    update

--- inversion_effciency.m
+++ inversion_effciency.m
@@ -3,9 +3,17 @@ close all;
 addpath ./data/
 filenames = dir(fullfile('./data', '*.IMA'));
 
-
-
 for i = 1:numel(filenames)
-    data(i).matrix = dicomread(filenames(i).name);
+    data(i,1).ima = dicomread(filenames(i).name);
 end
+
+
+for i = 1:16:size(data,1)
+    figure;
+    for j = 1:16
+        subplot(4,4,j);
+        imshow(data(j+i-1).ima, []);
+    end
+end
+
 
