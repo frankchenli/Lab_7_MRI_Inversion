81434f676c3788f05f8d88fc9475e96ba2da844c
frank <chl179@ucsd.edu>
2024-03-04 22:35:05 -0800

    Update inversion_effciency.m

--- inversion_effciency.m
+++ inversion_effciency.m
@@ -4,19 +4,37 @@ addpath ./data/
 filenames = dir(fullfile('./data', '*.IMA'));
 
 for i = 1:numel(filenames)
-    data(i,1).ima = dicomread(filenames(i).name);
-    data(i,1).info = dicominfo(filenames(i).name);
-
-
+    data(i).ima = dicomread(filenames(i).name);
+    data(i).info = dicominfo(filenames(i).name);
+    data(i).series = data(i).info.SeriesDescription;
 end
 %%
 
-for i = 1:16:size(data,1)
+
+oil = [];
+perf = [];
+BIRN = [];
+for i = 1:size(data,2)
+    if contains(data(i).series,'OilPhan') == 1
+        oil = [oil i];
+    elseif contains(data(i).series,'Perfusion_Weighted') == 1
+        perf = [perf i];
+    elseif contains(data(i).series,'BIRNPhan') == 1
+        BIRN = [BIRN i];
+    end
+end
+
+oilphan = data(oil); perfusion = data(perf); birnphan = data(BIRN);
+
+
+for i = 1:16:size(birnphan,2)
     figure;
     for j = 1:16
         subplot(4,4,j);
-        imshow(data(j+i-1).ima, []);
+        imshow(birnphan(j+i-1).ima, []);
     end
 end
+
+
 
 
