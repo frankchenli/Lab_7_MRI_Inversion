22e978666265f14544a810bb13912f082755ed3f
frank <chl179@ucsd.edu>
2024-03-18 15:58:36 -0700

    Update inversion_effciency.m

--- inversion_effciency.m
+++ inversion_effciency.m
@@ -1,3 +1,4 @@
+%%
 clear all;
 close all;
 addpath ./data/
@@ -8,7 +9,6 @@ for i = 1:numel(filenames)
     data(i).info = dicominfo(filenames(i).name);
     data(i).series = data(i).info.SeriesDescription;
 end
-%%
 
 
 oil = [];
@@ -26,25 +26,127 @@ end
 
 oilphan = data(oil); perfusion = data(perf); birnphan = data(BIRN);
 
-
+a = 1;
+for i = 11:3:47
+    oil_sech(a).img = double(oilphan(i).ima);
+    oil_sech(a).reference = double(oilphan(i-1).ima);
+    oil_sech(a).series = oilphan(i).series;
+    oil_sech(a).diff = oil_sech(a).reference-oil_sech(a).img
+    a = a+1;
+end
+oil_sech(14).img = double(oilphan(8).ima);
+oil_sech(14).reference = double(oilphan(7).ima);
+oil_sech(14).series = oilphan(8).series;
+oil_sech(14).diff = oil_sech(14).reference-oil_sech(14).img
 
 a = 1;
-for i = 8:3:47
-    oil_sech(a).img = oilphan(i).ima;
-    oil_sech(a).info = oilphan(i).info;
+for i = 50:3:65
+    oil_sinc(a).img = double(oilphan(i).ima);
+    oil_sinc(a).reference =  double(oilphan(i+1).ima);
+    oil_sinc(a).series = oilphan(i).series;
+    oil_sinc(a).diff = oil_sinc(a).reference-oil_sinc(a).img;
+    a = a+1;
+end
+
+a = 1;
+for i = 2:3:41
+    birn_sech(a).img =  double(birnphan(i).ima);
+    birn_sech(a).reference =  double(birnphan(i-1).ima);
+    birn_sech(a).series = birnphan(i).series;
+    birn_sech(a).diff = birn_sech(a).reference-birn_sech(a).img;
+    a = a+1;
+end
+
+a = 1;
+for i = 44:3:59
+    birn_sinc(a).img =  double(birnphan(i).ima);
+    birn_sinc(a).reference = double(birnphan(i+1).ima);
+    birn_sinc(a).series = birnphan(i).series;
+    birn_sinc(a).diff = birn_sinc(a).reference-birn_sinc(a).img
     a = a+1;
 end
 
 
 
-
-for i = 1:2:13
-    figure;
-    subplot(1,2,1);
-    imshow(oil_sech(i).img,[]);
-    subplot(1,2,2);
-    imshow(oil_sech(i+1).img,[]);
+% figure;
+% subplot(1,3,1)
+% imshow(oil_sech(5).reference,[]);
+% title('Non-inverted');
+% subplot(1,3,2);
+% imshow(oil_sech(5).img,[])
+% title('Inverted');
+% subplot(1,3,3);
+% imshow(oil_sech(5).diff,[]);
+% title('Difference (Non inverted-Inverted)');
+% hold on
+% plot([64 64], [1 128], 'r');
+%%
+close all
+titles = ["B1 = 0.0",'B1 = 0.05','B1 = 0.1','B1 = 0.15','B1 = 0.25','B1 = 0.35','B1 = 0.45','B1 = 0.55','B1 = 0.65','B1 = 0.75','B1 = 0.85','B1 = 0.95','B1 = 1.05','B1 = 1.15'];
+for i = 1:size(oil_sech,2)
+        figure;
+        % subplot(2,7,i);
+        % stem(mean(oil_sech(i).img(44:95,41:83),2));
+        subplot(2,1,1)
+        stem(oil_sech(i).diff(:,64))
+        ylim([0 300])
+        title(titles(i));
+        subplot(2,1,2)
+        stem(oil_sech(i).img(:,64))
+        % imshow(oil_sech(i).img,[]);
 end
+% sgtitle('Oil phantom Adiabatic Difference(Non inverted - Inverted)')
+
+% figure;
+% subplot(3,3,1)
+% stem(oil_sech(4).diff(:,64))
+% ylim([0 300])
+% title('B1 = 0.15 Difference(reference-Inverted)');
+% subplot(3,3,2)
+% stem(oil_sech(5).diff(:,64));
+% ylim([0 300])
+% title('B1 = 0.25 Difference(reference-Inverted)');
+% subplot(3,3,3)
+% stem(oil_sech(6).diff(:,64));
+% ylim([0 300])
+% title('B1 = 0.35 Difference(reference-Inverted)');
+% subplot(3,3,4)
+% stem(oil_sech(4).img(:,64));
+% ylim([0 500])
+% title('B1 = 0.15 Inverted');
+% subplot(3,3,5)
+% stem(oil_sech(5).img(:,64));
+% ylim([0 500])
+% title('B1 = 0.25 Inverted');
+% subplot(3,3,6)
+% stem(oil_sech(6).img(:,64));
+% ylim([0 500])
+% title('B1 = 0.35 Inverted');
+% subplot(3,3,7)
+% stem(oil_sech(4).reference(:,64));
+% ylim([0 500])
+% title('B1 = 0.15 reference');
+% subplot(3,3,8)
+% stem(oil_sech(5).reference(:,64));
+% ylim([0 500])
+% title('B1 = 0.25 reference');
+% subplot(3,3,9)
+% stem(oil_sech(6).reference(:,64));
+% ylim([0 500])
+% title('B1 = 0.35 reference');
+
+
+
+
+
+% 
+% for i = 1:2:13
+%     figure;
+%     subplot(1,2,1);
+%     imshow(oil_sech(i).img,[]);
+%     subplot(1,2,2);
+%     imshow(oil_sech(i+1).img,[]);
+% end 
 
 
 
